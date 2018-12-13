6fe803822cc6160faf44a574a339eb1d7f0502a6
Mateu Cànaves <mateu.canaves@gmail.com>
2018-12-13 17:17:35 +0100

    Add middleware

---
+++ hr_prometheus/middleware.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+import prometheus_client
+from aiohttp.web import middleware
+
+from hr_prometheus.request_monitor import RequestMonitor
+
+
+def hrprometheus_middleware(request_monitor: Optional[RequestMonitor] = None):
+    @middleware
+    async def middleware_handler(request, handler):
+        request_monitor = request_monitor or RequestMonitor(request)
+        with request_monitor(request) as monitor:
+            response = await handler(request)
+            monitor.observe(response)
+            return response
+
+    return middleware_handler
