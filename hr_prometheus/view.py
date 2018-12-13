c11f814e947ec5df52b70b8ea665e84f67437b0c
Mateu Cànaves <mateu.canaves@gmail.com>
2018-12-13 17:17:51 +0100

    Add metrics view

---
+++ hr_prometheus/view.py
@@ -0,0 +1,8 @@
+from aiohttp.web import Response
+from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
+
+
+async def hrprometheus_metrics(request):
+    resp = Response(body=generate_latest())
+    resp.content_type = CONTENT_TYPE_LATEST
+    return resp
