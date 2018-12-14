c81ae30be3e441420156475540942c96d48896f0
Mateu Cànaves <mateu.canaves@gmail.com>
2018-12-14 13:12:19 +0100

    Fix middleware erros

--- hr_prometheus/middleware.py
+++ hr_prometheus/middleware.py
@@ -1,13 +1,13 @@
 from aiohttp.web import middleware
 
-from hr_prometheus.request_monitor import RequestMonitor
+from hr_prometheus.monitors import RequestMonitor
 
 
 def hrprometheus_middleware(request_monitor=None):
     @middleware
     async def middleware_handler(request, handler):
-        request_monitor = request_monitor or RequestMonitor(request)
-        with request_monitor(request) as monitor:
+        request_manager = request_monitor or RequestMonitor
+        with request_manager(request) as monitor:
             response = await handler(request)
             monitor.observe(response)
             return response
