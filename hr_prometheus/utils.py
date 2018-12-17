bcd71bb2ad572608925ad36e1c5494ebb48e4dbc
Mateu Cànaves <mateu.canaves@gmail.com>
2018-12-17 16:36:33 +0100

    Add context decorator timing util

--- hr_prometheus/utils.py
+++ hr_prometheus/utils.py
@@ -1,8 +1,23 @@
+from contextlib import ContextDecorator
 from enum import Enum
+from time import time
 
 
-def apply_labels(metric, labels):
-    resolved_labels = [
-        label.value if isinstance(label, Enum) else label for label in labels
-    ]
-    return metric.labels(*resolved_labels)
+class TimeMonitor(ContextDecorator):
+    def __init__(self, metric, labels):
+        self.metric = metric
+        self.labels = labels
+        self.init_time = None
+
+    def __enter__(self):
+        self.init_time = time()
+        return self
+
+    def __exit__(self, *exc):
+        self._apply_labels().observe(time() - self.init_time)
+
+    def _apply_labels(self):
+        resolved_labels = [
+            label.value if isinstance(label, Enum) else label for label in self.labels
+        ]
+        return self.metric.labels(*resolved_labels)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 import pytest
 
-from hr_prometheus.utils import apply_labels
+from hr_prometheus.utils import TimeMonitor
 
 
 class EnumLabels(Enum):
@@ -18,7 +18,49 @@ class EnumLabels(Enum):
         (["label1", EnumLabels.LABEL2], ["label1", "label2"]),
     ],
 )
-def test_apply_labels(labels, expected, mocker):
+def test_decorator(labels, expected, mocker):
+    mocker.patch("hr_prometheus.utils.time", return_value=3.141_592_653_59)
     metric_mock = mocker.Mock()
-    apply_labels(metric_mock, labels)
+    context = TimeMonitor(metric_mock, labels)
+
+    @context
+    def test():
+        assert context.init_time == 3.141_592_653_59
+
+    test()
     metric_mock.labels.assert_called_once_with(*expected)
+    metric_mock.labels.return_value.observe.assert_called_once_with(0)
+
+
+async def test_decorator_async(mocker):
+    mocker.patch("hr_prometheus.utils.time", return_value=3.141_592_653_59)
+    metric_mock = mocker.Mock()
+    context = TimeMonitor(metric_mock, ["label1"])
+
+    @context
+    async def test():
+        assert context.init_time == 3.141_592_653_59
+
+    await test()
+    metric_mock.labels.assert_called_once_with(*["label1"])
+    metric_mock.labels.return_value.observe.assert_called_once_with(0)
+
+
+@pytest.mark.parametrize(
+    "labels,expected",
+    [
+        (["label1"], ["label1"]),
+        ([EnumLabels.LABEL1], ["label1"]),
+        (["label1", EnumLabels.LABEL2], ["label1", "label2"]),
+    ],
+)
+def test_contextmanager(labels, expected, mocker):
+    mocker.patch("hr_prometheus.utils.time", return_value=3.141_592_653_59)
+    metric_mock = mocker.Mock()
+    context = TimeMonitor(metric_mock, labels)
+
+    with context as t:
+        assert t.init_time == 3.141_592_653_59
+
+    metric_mock.labels.assert_called_once_with(*expected)
+    metric_mock.labels.return_value.observe.assert_called_once_with(0)
