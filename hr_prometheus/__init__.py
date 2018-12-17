ae23e83c948f21388758ec05f3db637348d6c360
Mateu Cànaves <mateu.canaves@gmail.com>
2018-12-17 18:01:35 +0100

    Preload required functions

--- hr_prometheus/__init__.py
+++ hr_prometheus/__init__.py
@@ -1,1 +1,5 @@
+from hr_prometheus.middleware import hrprometheus_middleware
+from hr_prometheus.view import hrprometheus_view
+
 __version__ = "0.0.1"
+__all__ = ["hrprometheus_middleware", "hrprometheus_view"]
