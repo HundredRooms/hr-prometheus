d5f0f4d22a5a19a6c0bd07181d8687f18218ee4c
Mateu Cànaves <mateu.canaves@gmail.com>
2018-12-18 10:29:34 +0100

    Add context decorator timing util (#4)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 import pytest
 
-from hr_prometheus.utils import apply_labels
+from hr_prometheus.utils import TimeMonitor, apply_labels
 
 
 class EnumLabels(Enum):
@@ -22,3 +22,53 @@ def test_apply_labels(labels, expected, mocker):
     metric_mock = mocker.Mock()
     apply_labels(metric_mock, labels)
     metric_mock.labels.assert_called_once_with(*expected)
+
+
+def test_decorator(mocker):
+    metric_mock = mocker.Mock()
+    mocker.patch("hr_prometheus.utils.time", return_value=3.141_592_653_59)
+    apply_labels_mock = mocker.patch(
+        "hr_prometheus.utils.apply_labels", return_value=metric_mock
+    )
+    context = TimeMonitor(metric_mock, ["label1"])
+
+    @context
+    def test():
+        assert context.init_time == 3.141_592_653_59
+        assert context.metric == metric_mock
+
+    test()
+    apply_labels_mock.assert_called_once_with(metric_mock, ["label1"])
+    metric_mock.observe.assert_called_once_with(0)
+
+
+async def test_decorator_async(mocker):
+    metric_mock = mocker.Mock()
+    mocker.patch("hr_prometheus.utils.time", return_value=3.141_592_653_59)
+    apply_labels_mock = mocker.patch(
+        "hr_prometheus.utils.apply_labels", return_value=metric_mock
+    )
+    context = TimeMonitor(metric_mock, ["label1"])
+
+    @context
+    async def test():
+        assert context.init_time == 3.141_592_653_59
+
+    await test()
+    apply_labels_mock.assert_called_once_with(metric_mock, ["label1"])
+    metric_mock.observe.assert_called_once_with(0)
+
+
+def test_contextmanager(mocker):
+    metric_mock = mocker.Mock()
+    mocker.patch("hr_prometheus.utils.time", return_value=3.141_592_653_59)
+    apply_labels_mock = mocker.patch(
+        "hr_prometheus.utils.apply_labels", return_value=metric_mock
+    )
+    context = TimeMonitor(metric_mock, ["label1"])
+
+    with context as t:
+        assert t.init_time == 3.141_592_653_59
+
+    apply_labels_mock.assert_called_once_with(metric_mock, ["label1"])
+    metric_mock.observe.assert_called_once_with(0)
