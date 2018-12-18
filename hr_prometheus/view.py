48becdc179feb1cd2bdb70f382e0392ccc438f91
Mateu Cànaves <mateu.canaves@gmail.com>
2018-12-18 14:20:16 +0100

    Docs (#5)

--- hr_prometheus/view.py
+++ hr_prometheus/view.py
@@ -2,7 +2,7 @@ from aiohttp.web import Response
 from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 
 
-async def hrprometheus_metrics(request):
+async def hrprometheus_view(request):
     resp = Response(body=generate_latest())
     resp.content_type = CONTENT_TYPE_LATEST
     return resp
