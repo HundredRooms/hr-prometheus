a636a56d7551586a2d398177fdefa34a2c1995c6
Jordi <jordi.solerbusquets@gmail.com>
2018-12-27 12:08:18 +0100

    Allow fixed routes parameters (#8)
    * Allow fixed route parameters
    
    * Update readme
    
    * make Black linter happy
    
    * Make isort happy
    
    * Update isort changes
    
    * Update version

--- hr_prometheus/__init__.py
+++ hr_prometheus/__init__.py
@@ -1,5 +1,5 @@
 from hr_prometheus.middleware import hrprometheus_middleware
 from hr_prometheus.view import hrprometheus_view
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __all__ = ["hrprometheus_middleware", "hrprometheus_view"]
